eda5875433d2f9491f2d174dfb2e7d108af2b5a9
tg1523342 <tg1523342@icloud.com>
2025-02-21 14:44:27 -0500

    Made indentation and string literal quotes consistant

--- public/JS/templates.js
+++ public/JS/templates.js
@@ -1,11 +1,11 @@
 const headerDiv = document.getElementById('header');
 const template = document.createElement('template');
 
-template.innerHTML = "<a href='/'><h1>Scout App</h1></a>";
+template.innerHTML = '<a href='/'><h1>Scout App</h1></a>';
 
 headerDiv.appendChild(template.content);
 
 const footerDiv = document.getElementById('footer');
 
-template.innerHTML = "<p>© Copyright 2025 Walled Lake Robotics</p>";
+template.innerHTML = '<p>© Copyright 2025 Walled Lake Robotics</p>';
 footerDiv.appendChild(template.content);
