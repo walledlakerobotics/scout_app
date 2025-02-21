676fd163298a8f7f55e4ce3b4555487a5f1a23ff
tg1523342 <tg1523342@icloud.com>
2025-02-21 15:45:10 -0500

    Cache all needed files and process requests

--- public/serviceWorker.js
+++ public/serviceWorker.js
@@ -6,20 +6,40 @@ const assets = [
     '/CSS/style.css',
     '/JS/app.js',
     '/JS/templates.js',
+    '/images/favicon.ico',
+    '/images/icon.png',
+    '/manifest.webmanifest'
 ];
 
+async function createCache() {
+    const cache = await caches.open(cacheName);
+    await cache.addAll(assets);
+    await self.skipWaiting();
+}
+
+async function processRequest(req) {
+    const res = await caches.match(req);
+    return res || await fetch(req);
+}
+
+async function clearOldCache() {
+    const keys = await caches.keys();
+    keys.forEach(async key => {
+        if (key != cacheName) {
+            await caches.delete(key);
+        }
+    });
+    await self.clients.claim();
+}
+
 self.addEventListener('install', event => {
-    event.waitUntil(
-        caches.open(cacheName)
-            .then(cache => cache.addAll(assets)
-            .then(self.skipWaiting())
-        )
-    );
+    event.waitUntil(createCache());
 });
 
 self.addEventListener('fetch', event => {
-    event.respondWith(
-        caches.match(event.request)
-            .then(res => res || fetch(event.request))
-    )
+    event.respondWith(processRequest(event.request))
+});
+
+self.addEventListener('activate', event => {
+    event.waitUntil(clearOldCache());
 });
