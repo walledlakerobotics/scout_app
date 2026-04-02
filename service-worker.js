53c63c86b29741693dc31cbe2b034b2cd7aee45b
BryceHeitzmann <109158364+Dialga156b@users.noreply.github.com>
2026-04-01 21:11:37 -0400

    misc

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "frc-scout-v7";
+const CACHE_NAME = "frc-scout-v8";
 //thisll have to change later, but right now big list needs to happen
 const ASSETS = ["/", "/manifest.json", "/HTML/scout.html", "/CSS/global.css", "/CSS/event-frc/event-frc.css", "/CSS/event-frc/scout.css", "/JS/theme.js", "/JS/DB.js", "/JS/event/scout-questions.js", "/JS/utils.js", "/JS/event/scout.js", "/JS/event/qrcode.js", "/Img/FIRST_mono.png"];
 
@@ -7,7 +7,6 @@ self.addEventListener("install", (e) => {
 });
 
 self.addEventListener("fetch", (e) => {
-  // Only handle GET requests - Cache API doesn't support POST/PUT/DELETE/etc.
   if (e.request.method !== "GET") return;
 
   e.respondWith(
